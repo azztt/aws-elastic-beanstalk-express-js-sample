2ff4536e76cbaa42758e62d96bb91e5fcc6ce5a1
azztt <anweshanbor@gmail.com>
2021-06-14 22:49:14 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi there, this was done by Anweshan'));
+app.get('/', (req, res) => res.send('Hi there, this was done to test manual review!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
